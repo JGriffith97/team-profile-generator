5981606d4c714abd30ab35bf3b0f5305369088f5
Jayden Griffith <griffithjayden97@outlook.com>
2022-06-25 14:41:53 -0600

    Imported all classes to the index.js file.

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const inquirer = require("inquirer");
+const Employee = require('./lib/Employee')
+const Engineer = require('./lib/Engineer')
+const Intern = require('./lib/Intern')
+const Manager = require('./lib/Manager')
 
 // Gonna need some cards, dynamically generated.
 // Array for questions? Or within the prompt?
