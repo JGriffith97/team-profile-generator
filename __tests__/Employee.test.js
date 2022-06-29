0bae48417ebcd46eba052ae4cd43f129324df1c7
Jayden Griffith <griffithjayden97@outlook.com>
2022-06-28 18:24:49 -0600

    Began intro work on the employee.test file.

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,1 +1,18 @@
-// What are we testing for?
+// Describe each of the tests for the employee
+// Positive, negative, exception.
+// Positive is normal functionality
+// negative is what can go wrong, to test against. No database or API calls in this case.
+// Exception, errors, in case of broken code.
+
+const Employee = require("../lib/Employee");
+
+describe('Employee', () => {
+  descrive('getName', () => {
+    it('should list the name of the employee specified by "this.name".')
+      const empName = "John";
+
+      const newEmp = new Employee("John", 1, "JohnJc@placeholder.com")
+
+      expect(newEmp.name).toEqual(empName)
+  })
+})
