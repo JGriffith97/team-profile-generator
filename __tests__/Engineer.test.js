f89286e53d7cb7459e3f5f924e8ae9c2cd575741
Jayden Griffith <griffithjayden97@outlook.com>
2022-06-29 19:30:52 -0600

    Altered Engineer.test to use a github username, rather than a URL

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -5,9 +5,9 @@ const Engineer = require('../lib/Engineer')
 describe('Engineer', () => {
   describe('getGithub', () => {
     it('should list the GitHub account specified by "this.github".', () => {
-      const empGitHub = "https://github.com/JGriffith97"
+      const empGitHub = "JGriffith97"
 
-      const newEng = new Engineer("Jayden", 2, "griffithjayden97@outlook.com", "https://github.com/JGriffith97")
+      const newEng = new Engineer("Jayden", 2, "griffithjayden97@outlook.com", "JGriffith97")
 
       expect(newEng.github).toEqual(empGitHub)
     })
